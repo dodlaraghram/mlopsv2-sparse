f415e3cf0a5e1ae43e92be3015e7a0ad8ee57278
dodlaraghram <113520166+dodlaraghram@users.noreply.github.com>
2023-10-09 20:05:37 +0530

    Update main.tf

--- infrastructure/modules/container-registry/main.tf
+++ infrastructure/modules/container-registry/main.tf
@@ -3,8 +3,8 @@ locals {
   safe_postfix = replace(var.postfix, "-", "")
 }
 
-resource "azurerm_container_registry" "cr" {
-  name                = "cr${local.safe_prefix}${local.safe_postfix}${var.env}"
+resource "azurerm_container_registry" "cra" {
+  name                = "cra${local.safe_prefix}${local.safe_postfix}${var.env}"
   resource_group_name = var.rg_name
   location            = var.location
   sku                 = "Standard"
